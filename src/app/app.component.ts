6c0fa2e1ed84cadfdac5dac480082d2ece1dd06a
unknown <th.david12@gmail.com>
2020-02-23 09:56:35 +0100

    Création de l'applicaiton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  tableauChoix = ['choix 1', 'choix 2', 'choix 3'];
 }
